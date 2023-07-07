80c36f9bb9b8ae1dea77027daedafd58cf0295f7
loyd ngei <loydngei@gmail.com>
2023-07-07 21:11:29 +0300

    Concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A type-annotated func concat that returns a str"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns concatenated string"""
+    return str1 + str2
