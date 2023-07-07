8b73a88aab066edf07a6d29d9fdff0382cf50c79
loyd ngei <loydngei@gmail.com>
2023-07-08 01:57:08 +0300

    Duck

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """Annotate the below function’s parameters"""
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Return values with the appropriate types"""
     return [(i, len(i)) for i in lst]
