80a6afdab46829e5a69105469a1e4a4f7cc7ca60
loyd ngei <loydngei@gmail.com>
2023-07-08 01:03:46 +0300

    List of floats

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,7 @@
 """Annotated func sum_list takes input_list"""
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
     """Returns their sum as a float"""
     return sum(input_list)
