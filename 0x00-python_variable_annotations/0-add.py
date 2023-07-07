6c404422697afdbb94f17ca162a9a16200d31c43
loyd ngei <loydngei@gmail.com>
2023-07-07 21:05:55 +0300

    Add Annotations

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Write a type-annotated function add. Returns sum"""
 
+
 def add(a: float, b: float) -> float:
+    """Function that returns sum of floats"""
     return a + b
