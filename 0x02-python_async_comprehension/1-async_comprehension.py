63a31e1e80f82bdfc1ee825ba5fde42a11ea6cbc
loyd ngei <loydngei@gmail.com>
2023-07-11 16:19:21 +0300

    Async generator

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Async coprehensions """
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Return 10 random numbers"""
+    outcome = [i async for i in async_generator()]
+    return outcome
