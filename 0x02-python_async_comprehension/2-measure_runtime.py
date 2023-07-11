1bbd4509b09a82a4ec91cc1bf4dde41b5c7ec58e
loyd ngei <loydngei@gmail.com>
2023-07-11 16:38:32 +0300

    Measure runtime

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Run time for parallel comprehensions"""
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """Measure the total runtime"""
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+    end_time = time.time()
+    return end_time - start_time
