881e529b7ed641ad180fa032a34c7b9e5765ef31
Loyd Ngei <loydngei@gmail.com>
2023-07-29 13:33:47 +0300

    Test utils

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Generic utilities for github org client."""
+import unittest
+from parameterized import parameterized
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json, memoize
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test that the method returns what it is supposed to"""
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2)
+        ])
+    def test_access_nested_map(self, nested_map, path, expected_output):
+        """Test that the method returns what it is supposed to"""
+        self.assertEqual(access_nested_map(nested_map, path), expected_output)
